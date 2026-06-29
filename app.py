3b17070f69a5327b71c0495ff697289d8be1a18e
manhquybmt <77561441+manhquybmt@users.noreply.github.com>
2026-06-29 22:01:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,20 @@
-from flask import Flask, request, jsonify
-from binance_bot import execute_order
-import os
+from flask import Flask
+from config import Config
+from database import db
+
+from dashboard.routes import dashboard_bp
+from api.webhook import webhook_bp
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-SECRET=os.getenv("WEBHOOK_SECRET")
+db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
+app.register_blueprint(dashboard_bp)
+app.register_blueprint(webhook_bp)
 
-    data=request.json
-
-    if data.get("secret") != SECRET:
-        return jsonify({"error":"unauthorized"}),401
-
-
-    result=execute_order(data)
-
-    return jsonify(result)
-
-
-
-@app.route('/')
-def home():
-    return "Trading Bot Running"
-
-
-if __name__=="__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
