cf79a115a0f6a5aa457820a7774b071492ad86d1
manhquybmt <77561441+manhquybmt@users.noreply.github.com>
2026-06-29 17:23:22 +0700

    Create binance_bot.py

---
+++ binance_bot.py
@@ -0,0 +1,52 @@
+from binance.client import Client
+import os
+
+
+client=Client(
+    os.getenv("BINANCE_KEY"),
+    os.getenv("BINANCE_SECRET")
+)
+
+
+SYMBOL=os.getenv("SYMBOL")
+QTY=float(os.getenv("QTY"))
+
+
+
+def execute_order(data):
+
+    action=data["action"]
+
+
+    if action=="LONG":
+
+        order=client.futures_create_order(
+            symbol=SYMBOL,
+            side="BUY",
+            type="MARKET",
+            quantity=QTY
+        )
+
+
+    elif action=="SHORT":
+
+        order=client.futures_create_order(
+            symbol=SYMBOL,
+            side="SELL",
+            type="MARKET",
+            quantity=QTY
+        )
+
+
+    elif action=="CLOSE":
+
+        order=client.futures_create_order(
+            symbol=SYMBOL,
+            side="SELL",
+            type="MARKET",
+            quantity=QTY,
+            reduceOnly=True
+        )
+
+
+    return order
